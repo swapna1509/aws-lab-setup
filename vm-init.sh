6f9eda7aef03137cfc269d56ca9acb9536be44e4
swapna1509 <39286385+swapna1509@users.noreply.github.com>
2018-05-25 12:29:54 +0530

    Create vm-init.sh

---
+++ vm-init.sh
@@ -0,0 +1,8 @@
+1. Raise SR requests
+2. Upload Key Pair
+3. Create Security Group
+4. Create EC2 Instance
+5. Setup YUM repos and INIT script.
+# yum install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+# curl https://raw.githubusercontent.com/cit-astrum/lab-setup/master/vm-init.sh | bash
+6. Create an AMI
